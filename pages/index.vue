8bbb5305082af9a7be7c26d6bb095dfab6260a45
santanagabi <gaboliveirasantana1@gmail.com>
2023-07-19 20:38:20 -0300

    created the strore: actions and mutations too

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,40 @@
 <template>
-  <div>xxxxxxxxx</div>
+  <main>
+    <h1>Task Board</h1>
+    <p>Create a list of tasks</p>
+
+    <div class="create-new">
+      <input 
+        type="text" 
+        v-model="newTask" 
+        placeholder="Add a new task" 
+        @keypress.enter="addTask" 
+      />
+      <button @click="addTask">Add Task</button>
+    </div>
+
+    <div class="tasks">
+      <!-- Component -->
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  data() {
+    return {
+      newTask: ''
+    }
+  },
+
+  methods: {
+    addTask() {
+      if(this.newTask) { // It´s not empty
+
+      }
+    }
+  }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -1,3 +1,24 @@
 // store = handle application data and states
-// TODO
-export const state = () => ({});
+
+// 1-) states
+export const state = () => ({
+  tasks: [],
+});
+
+// 2-) mutations
+// change the state
+export const mutations = {
+  ADD_TASK(state, task) {
+    // create a new task and join it with the existing ones
+    state.tasks = [{ content: task, done: false }, ...state.tasks];
+    // state.tasks.push({ content: task, done: false });
+    // state.tasks.unshift({ conten: task, done: false });
+  },
+  REMOVE_TASK(state, task) {
+    // state.tasks.splice(state.tasks.indexOf(task), 1);
+    state.tasks = state.tasks.filter((item) => item !== task);
+  },
+  TOGGLE_TASK(state, task) {
+    task.done = !task.done;
+  },
+};
